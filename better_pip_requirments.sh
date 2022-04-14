bec78663ada29d3215d1780eee827581116f2a1d
Kakan Ghosh <kakanghosh69@gmail.com>
2022-04-14 20:35:50 +0600

    Added uninstall and ls command

--- better_pip_requirments.sh
+++ better_pip_requirments.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 kgpip() {
-
+    DEP_FILE_NAME="requirments.txt"
     install_package() {
-        DEP_FILE_NAME="requirments.txt"
         package_name=$1
         echo "Package $(tput setaf 2)$package_name$(tput sgr0) is installing..."
         # installing the actual package
@@ -11,12 +10,36 @@ kgpip() {
         # check if package is already added in the dependency file
         found=0
         if [ -f "$DEP_FILE_NAME" ]; then
-            found=$(cat $DEP_FILE_NAME | grep -c "$package_name")
+            found=$(grep -c "$package_name" $DEP_FILE_NAME)
         fi
+
         if [ "$found" == 0 ]; then
             # adding the new package in the dependency file in the freeze output form if not exist
             pip freeze | grep -i "$package_name" >>"$DEP_FILE_NAME"
-            echo "Installation is completed and added to $(tput setaf 3)$DEP_FILE_NAME$(tput sgr0)"
+            found=$(grep -c "$package_name" $DEP_FILE_NAME)
+            if [ "$found" == 1 ]; then
+                echo "Installation is completed and added to $(tput setaf 3)$DEP_FILE_NAME$(tput sgr0)"
+            fi
+        fi
+    }
+
+    uninstall_package() {
+        package_name=$1
+        echo "Package $(tput setaf 2)$package_name$(tput sgr0) is uninstalling..."
+        # uninstalling the actual package
+        pip uninstall "$package_name" -y
+        if [ -f "$DEP_FILE_NAME" ]; then
+            # check if package is present in the dependency file
+            found=$(grep -c "$package_name" $DEP_FILE_NAME)
+            if [ "$found" -gt 0 ]; then
+                number_of_other_package=$(grep -v "$package_name" $DEP_FILE_NAME | grep -c $DEP_FILE_NAME)
+                if [ "$number_of_other_package" -gt 0 ]; then
+                    grep -v "$package_name" $DEP_FILE_NAME >${DEP_FILE_NAME}.bak &&
+                        mv ${DEP_FILE_NAME}.bak $DEP_FILE_NAME
+                else
+                    echo -n "" >$DEP_FILE_NAME
+                fi
+            fi
         fi
     }
 
@@ -24,10 +47,22 @@ kgpip() {
         package_list=("$@")
         no_of_arguments=$#
         if [ "$no_of_arguments" == 0 ]; then
-            echo "$(tput setaf 1)"Package name is required!"$(tput sgr0)"
+            pip install -r "$DEP_FILE_NAME"
         else
             for package_name in "${package_list[@]}"; do
                 install_package "$package_name"
+            done
+        fi
+    }
+
+    uninstall_packages() {
+        package_list=("$@")
+        no_of_arguments=$#
+        if [ "$no_of_arguments" == 0 ]; then
+            echo "$(tput setaf 1)"Package name is required!"$(tput sgr0)"
+        else
+            for package_name in "${package_list[@]}"; do
+                uninstall_package "$package_name"
             done
         fi
     }
@@ -41,8 +76,11 @@ kgpip() {
         'install')
             install_packages "${arguments[@]:1}"
             ;;
-        'remove' | 3)
-            echo "Package is removing"
+        'uninstall')
+            uninstall_packages "${arguments[@]:1}"
+            ;;
+        'list' | 'ls')
+            cat "$DEP_FILE_NAME"
             ;;
         *)
             echo "Invalid command"
